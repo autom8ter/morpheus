09a2483372537fe0e79c01c5114cc24d04eb97a0
Coleman Word <colemanword@gmail.com>
2022-03-11 19:52:16 -0700

    fixes

--- pkg/persistance/persist.go
+++ pkg/persistance/persist.go
@@ -20,11 +20,12 @@ func NewPersistantGraph(dir string, cacheSize int) (api.Graph, error) {
 		return nil, err
 	}
 	cache, err := lru.NewWithEvict(cacheSize, func(key interface{}, value interface{}) {
-		if value.(*cachedItem).commited {
+		item := value.(*cachedItem)
+		if item.commited {
 			return
 		}
 		if err := db.Update(func(txn *badger.Txn) error {
-			bits, _ := encode.Marshal(value)
+			bits, _ := encode.Marshal(item)
 			if err := txn.Set([]byte(key.(string)), bits); err != nil {
 				return stacktrace.Propagate(err, "failed to set key: %s", key)
 			}
@@ -41,50 +42,57 @@ func NewPersistantGraph(dir string, cacheSize int) (api.Graph, error) {
 		if properties == nil {
 			properties = map[string]interface{}{}
 		}
-		cache.Add(compoundKey, &cachedItem{
-			properties: properties,
-			commited:   false,
-		})
-		return api.NewEntity(
+		e := api.NewEntity(
 			nodeType,
 			nodeID,
-			func() map[string]interface{} {
-				if val, ok := cache.Get(compoundKey); ok {
-					return val.(*cachedItem).properties
-				}
-
-				data := map[string]interface{}{}
-
-				if err := db.View(func(txn *badger.Txn) error {
-					val, err := txn.Get([]byte(compoundKey))
-					if err != nil {
-						return stacktrace.Propagate(err, "failed to get key")
-					}
-					if err := val.Value(func(val []byte) error {
-						return encode.Unmarshal(val, &data)
-					}); err != nil {
-						return err
-					}
-					cache.Add(compoundKey, &cachedItem{
-						properties: data,
-						commited:   true,
-					})
-					return nil
-				}); err != nil {
-					panic(err)
-				}
-				return data
-			},
-			func(properties map[string]interface{}) {
-				if properties == nil {
-					properties = map[string]interface{}{}
-				}
-				cache.Add(compoundKey, properties)
-			})
+			get(db, cache, compoundKey),
+			set(cache, compoundKey),
+		)
+		e.SetProperties(properties)
+		return e
 	}, func() error {
 		cache.Purge()
 		return db.Close()
 	}), nil
+}
+
+func set(cache *lru.Cache, compoundKey string) func(props map[string]interface{}) {
+	return func(props map[string]interface{}) {
+		cache.Add(compoundKey, &cachedItem{
+			properties: props,
+			commited:   false,
+		})
+	}
+}
+
+func get(db *badger.DB, cache *lru.Cache, compoundKey string) func() map[string]interface{} {
+	return func() map[string]interface{} {
+		if val, ok := cache.Get(compoundKey); ok {
+			return val.(*cachedItem).properties
+		}
+
+		data := map[string]interface{}{}
+
+		if err := db.View(func(txn *badger.Txn) error {
+			val, err := txn.Get([]byte(compoundKey))
+			if err != nil {
+				return stacktrace.Propagate(err, "failed to get key")
+			}
+			if err := val.Value(func(val []byte) error {
+				return encode.Unmarshal(val, &data)
+			}); err != nil {
+				return err
+			}
+			cache.ContainsOrAdd(compoundKey, &cachedItem{
+				properties: data,
+				commited:   true,
+			})
+			return nil
+		}); err != nil {
+			panic(err)
+		}
+		return data
+	}
 }
 
 func getKey(prefix, nodeType, nodeID string) string {
